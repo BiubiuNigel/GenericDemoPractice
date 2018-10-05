93850fa9670c16e908c2b8c7db5a7eb8455590e3
NigelChen <nigelc223@sina.com>
2018-10-05 19:56:47 +0800

    Add new class

---
+++ src/com/nigel/genericDemo/GenericDemo08.java
@@ -0,0 +1,10 @@
+package com.nigel.genericDemo;
+
+public class GenericDemo08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just a github update test");
+	}
+
+}
